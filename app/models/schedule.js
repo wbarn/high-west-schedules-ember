829eba7bf74ce5091efbaec483259e2e37439331
Will Barnette <will@wbarnette.com>
2015-07-28 13:49:19 -0600

    Add createdAt field

--- app/models/schedule.js
+++ app/models/schedule.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  notes: DS.attr('string')
+  notes: DS.attr('string'),
+  createdAt: DS.attr('date')
 });
