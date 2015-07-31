3f7693d712298e49d12adf93ac2679e8ce52b9c7
Will Barnette <will@wbarnette.com>
2015-07-31 12:57:11 -0600

    Add some more liquid-fire transitions

--- app/models/event.js
+++ app/models/event.js
@@ -1,9 +1,19 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   date: DS.attr('utc'),
   notes: DS.attr('string'),
 
+  schedule: DS.belongsTo('schedule'),
   productionItems: DS.hasMany('production-items'),
-  processingItems: DS.hasMany('processing-items')
+  processingItems: DS.hasMany('processing-items'),
+
+  productionitemsCount: Ember.computed('productionItems.length', function() {
+    return this.get('productionItems.length');
+  }),
+
+  processingItemsCount: Ember.computed('processingItems.length', function() {
+    return this.get('processingItems.length');
+  })
 });
--- app/transitions.js
+++ app/transitions.js
@@ -1,7 +1,16 @@
 export default function() {
+  // Schedule <-> Event
+  this.transition(
+    this.fromRoute('schedules.show.index'),
+    this.toRoute('schedules.show.events'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  // Editable rows
   this.transition(
     this.hasClass('editable-row'),
     this.toValue(true),
-    this.useAndReverse('crossFade')
+    this.useAndReverse('fade', { duration: 200 })
   );
 }
